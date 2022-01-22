d340c72f83287a3d0db5a6a839d905152ae4218a
ofujino <93305973+ofujino@users.noreply.github.com>
2022-01-22 18:43:53 +0900

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import { createShaderProgram, radians, createLoop } from './utils'
+import { createShader, radians, createLoop } from './utils'
 import { mat4 } from 'gl-matrix'
 //import 'webgl-lint'
 import vs from './shader.vs?raw'
@@ -23,27 +23,23 @@ window.onload = () => {
   const mView = mat4.create()
   const mModel = mat4.create()
 
-  mat4.perspective(mProjection, radians(45), canvas.width/canvas.height, 0.001, 100)
+  const aspect = canvas.width/canvas.height
+  mat4.perspective(mProjection, radians(45), aspect, 0.001, 100)
   mat4.lookAt(mView, [0,0,5], [0,0,0], [0,1,0])
 
-  const program = createShaderProgram(gl, vs.trim(), fs.trim())
-
-  const location = {}
-  location.mProjection = gl.getUniformLocation(program, 'mProjection')
-  location.mView = gl.getUniformLocation(program, 'mView')
-  location.mModel = gl.getUniformLocation(program, 'mModel')
+  const shader = createShader(gl, vs.trim(), fs.trim())
 
   const render = () => {
     mat4.rotateY(mModel, mModel, radians(0.5))
 
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-    gl.useProgram(program)
-    gl.uniformMatrix4fv(location.mProjection, false, mProjection)
-    gl.uniformMatrix4fv(location.mView, false, mView)
-    gl.uniformMatrix4fv(location.mModel, false, mModel)
+    shader.begin()
+    shader.setMatrix4('mProjection', mProjection)
+    shader.setMatrix4('mView', mView)
+    shader.setMatrix4('mModel', mModel)
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
-    gl.useProgram(null)
+    shader.end()
   }
 
   const loop = createLoop(() => render())
--- utils.js
+++ utils.js
@@ -33,6 +33,83 @@ export const createProgram = (gl, vertexShader, fragmentShader) => {
   return program
 }
 
+export const createArrayBuffer = (gl) => {
+  const buffer = {}
+  const type = gl.ARRAY_BUFFER
+  buffer.bufferObject = gl.createBuffer()
+  buffer.begin = function () {
+    gl.bindBuffer(type, this.bufferObject)
+    return this
+  }
+  buffer.end = function () {
+    gl.bindBuffer(type, null)
+    return this
+  }
+  buffer.setSize = function (size, usage) {
+    gl.bufferData(type, size, usage)
+    return this
+  }
+  buffer.setData = function (data, usage) {
+    gl.bufferData(type, data, usage)
+    return this
+  }
+  buffer.updateData = function (offset, data) {
+    gl.bufferSubData(type, offset, data)
+    return this
+  }
+  return buffer
+}
+
+export const createShader = (gl, vs, fs) => {
+  const shader = {}
+  shader.program = createShaderProgram(gl, vs.trim(), fs.trim())
+  shader.location = getLocations(gl, shader.program)
+  shader.begin = function () {
+    gl.useProgram(this.program)
+    return this
+  }
+  shader.end = function () {
+    gl.useProgram(null)
+    return this
+  }
+  shader.setMatrix4 = function (name, value) {
+    gl.uniformMatrix4fv(this.location[name], false, value)
+    return this
+  }
+  shader.enableAttribute = function (name) {
+    gl.enableVertexAttribArray(this.location[name])
+    return this
+  }
+  shader.disableAttribute = function (name) {
+    gl.disableVertexAttribArray(this.location[name])
+    return this
+  }
+  shader.setAttributeArray = function (name, size, type, normalized, stride, offset) {
+    gl.vertexAttribPointer(this.location[name], size, type, false, stride, offset)
+    return this
+  }
+  shader.setAttribute3fv = function (name, value) {
+    gl.vertexAttrib3fv(this.location[name], value)
+    return this
+  }
+  return shader
+}
+
+const getLocations = (gl, program) => {
+  const locations = {}
+  const attributes = gl.getProgramParameter(program, gl.ACTIVE_ATTRIBUTES)
+  for (let i = 0; i < attributes; i++) {
+    const info = gl.getActiveAttrib(program, i)
+    locations[info.name] = gl.getAttribLocation(program, info.name)
+  }
+  const uniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS)
+  for (let i = 0; i < uniforms; i++) {
+    const info = gl.getActiveUniform(program, i)
+    locations[info.name] = gl.getUniformLocation(program, info.name)
+  }
+  return locations
+}
+
 export const createLoop = (callback) => {
   const loop = {}
   loop.animation = null
